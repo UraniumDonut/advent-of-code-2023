6a6dde6a5f94c2fba590c4189f810ef82670468c
Leon Bartle <leon@bartle.eu>
2023-12-02 13:20:42 +0100

    day 1

---
+++ day-01/src/bin/part1.rs
@@ -0,0 +1,42 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+
+fn part1(input: &str) -> String {
+    let output = input
+        .lines()
+        .map(|line: &str| {
+            let mut it =
+                line.chars().filter_map(|c| {c.to_digit(10)
+                });
+            let first =
+                it.next().expect("should be a number");
+
+            match it.last(){
+                Some(num) => format!("{first}{num}"),
+                None => format!("{first}{first}"),
+            }
+            .parse::<u32>()
+            .expect("should be a number")
+        })
+        .sum::<u32>();
+
+    output.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = part1("1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet");
+        assert_eq!(result, "142".to_string());
+    }
+}
